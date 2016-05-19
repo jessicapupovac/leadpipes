5fe44d4243f90062945c49d3dc4f0a73f369b418
David Eads <davideads@gmail.com>
2016-05-19 16:49:54 -0400

    add simplistic geolocation

--- www/js/app.js
+++ www/js/app.js
@@ -2,8 +2,14 @@
 var active;
 var routes;
 var router;
+var cityReferences;
+var lang = 'en';
 
 var onDocumentLoad = function(e) {
+    cityReferences = document.getElementsByClassName('geo-city');
+
+    geoLocate();
+
     var routes = {
         '/:cardID': navigateToCard
     }
@@ -18,6 +24,27 @@ var navigateToCard = function(cardID) {
     var nextCard = document.getElementById(cardID);
     nextCard.classList.add('active');
     active = nextCard;
+}
+
+var geoLocate = function() {
+    // @TODO wrap in block that caches this stuff
+
+    if (typeof geoip2 === 'object') {
+        geoip2.city(onLocateIP, onLocateFail);
+    }
+}
+
+var onLocateIP = function(response) {
+    console.log('locate ip successful');
+    console.log(response);
+    for (var i = 0; i < cityReferences.length; ++i) {
+        var item = cityReferences[i];
+        item.innerHTML = response.city.names[lang] + ', ' + response.most_specific_subdivision.iso_code;
+    }
+}
+
+var onLocateFail = function(response) {
+    console.log('locate failed');
 }
 
 document.addEventListener('DOMContentLoaded', onDocumentLoad);
