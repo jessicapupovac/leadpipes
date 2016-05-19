96131183ab42b20c586ee0b2e822b4b7fe37976e
David Eads <davideads@gmail.com>
2016-05-19 15:41:30 -0400

    add header and css tweaks

--- www/js/app.js
+++ www/js/app.js
@@ -1,21 +1,23 @@
+// Global variables
 var active;
 var routes;
 var router;
 
 var onDocumentLoad = function(e) {
     var routes = {
-      '/:cardID': navigateToCard
+        '/:cardID': navigateToCard
     }
     router = Router(routes);
     router.init([COPY.content.initial_card]);
 }
 
 var navigateToCard = function(cardID) {
+    if (active) {
+        active.classList.remove('active');
+    }
     var nextCard = document.getElementById(cardID);
-    if (active) active.classList.remove('active');
     nextCard.classList.add('active');
     active = nextCard;
-    // @TODO ADD ANALYTICS STUFF
 }
 
 document.addEventListener('DOMContentLoaded', onDocumentLoad);
