faa632e1759100c0720ac005b0851af2481e3f2a
David Eads <davideads@gmail.com>
2016-06-02 09:49:16 -0400

    two-way session ID generation

--- lambda_functions/LeadPipesGenerateSessionID/lambda_function.py
+++ lambda_functions/LeadPipesGenerateSessionID/lambda_function.py
@@ -1,8 +1,20 @@
+import boto3
+import time
 import uuid
 
 
 def lambda_handler(event, context):
     """
-    Return a UUID.
+    Set and return a UUID.
     """
-    return unicode(uuid.uuid4())
+    id = unicode(uuid.uuid4())
+
+    client = boto3.client('dynamodb')
+    client.put_item(
+        TableName='LeadPipesSession',
+        Item={
+            'sessionid': {'S': id},
+            'timestamp': {'N': unicode(time.time())},
+        })
+
+    return id
